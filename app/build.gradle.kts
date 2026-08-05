ddd47e380c7d77239720ab420800f6bcaaa3f80b
Your Name <qianxuntudou@gmail.com>
2026-08-05 20:51:40 +0800

    v1.6.10

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -53,8 +53,8 @@ android {
         applicationId = "com.tutu.myblbl"
         minSdk = 23
         targetSdk = 35
-        versionCode = 69
-        versionName = "1.6.9"
+        versionCode = 70
+        versionName = "1.6.10"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }

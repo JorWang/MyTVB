643df6c7ef5e63bbe38ef3d56755ecd098b814cf
qianxuntudou-ops <qianxuntudou@gmail.com>
2026-09-09 09:23:45 +0800

    v1.6.23

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -54,8 +54,8 @@ android {
         applicationId = (project.findProperty("applicationId") as? String) ?: "com.tutu.myblbl"
         minSdk = 23
         targetSdk = 35
-        versionCode = 82
-        versionName = "1.6.22"
+        versionCode = 83
+        versionName = "1.6.23"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }

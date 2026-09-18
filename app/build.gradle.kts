72470451effda9a4e13daddd610a5e28972c047b
Your Name <qianxuntudou@gmail.com>
2026-09-18 22:58:41 +0800

    v2.0.4

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -54,8 +54,8 @@ android {
         applicationId = (project.findProperty("applicationId") as? String) ?: "com.mytvb"
         minSdk = 23
         targetSdk = 35
-        versionCode = 87
-        versionName = "2.0.3"
+        versionCode = 88
+        versionName = "2.0.4"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
